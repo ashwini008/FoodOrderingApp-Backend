63cefc7a3f3f8519df23a8c4b24163712e93b75d
Soumya Ranjan Tripathy <sibun.soumya@gmail.com>
2021-05-31 20:58:55 +0530

    updated categoryService

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/CategoryService.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/CategoryService.java
@@ -4,10 +4,12 @@ import com.upgrad.FoodOrderingApp.service.dao.CategoryDao;
 import com.upgrad.FoodOrderingApp.service.dao.RestaurantCategoryDao;
 import com.upgrad.FoodOrderingApp.service.dao.RestaurantDao;
 import com.upgrad.FoodOrderingApp.service.entity.CategoryEntity;
+import com.upgrad.FoodOrderingApp.service.entity.RestaurantCategoryEntity;
 import com.upgrad.FoodOrderingApp.service.entity.RestaurantEntity;
 import com.upgrad.FoodOrderingApp.service.exception.CategoryNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class CategoryService {
@@ -29,13 +31,25 @@ public class CategoryService {
     */
     public List<CategoryEntity> getCategoriesByRestaurant (String restaurantUuid) {
 
-        RestaurantEntity restaurantEntity = restaurantDao.getRes
+        //Calls getRestaurantByUuid of restaurantDao to get RestaurantEntity
+        RestaurantEntity restaurantEntity = restaurantDao.getRestaurantByUuid(restaurantUuid);
+
+        //Calls getCategoriesByRestaurant of restaurantCategoryDao to get list of RestaurantCategoryEntity
+        List<RestaurantCategoryEntity> restaurantCategoryEntities = restaurantCategoryDao.getCategoriesByRestaurant(restaurantEntity);
+
+        //Creating the list of the Category entity to be returned.
+        List<CategoryEntity> categoryEntities = new LinkedList<>();
+        restaurantCategoryEntities.forEach(restaurantCategoryEntity -> {
+            categoryEntities.add(restaurantCategoryEntity.getCategory());
+        });
+        return categoryEntities;
     }
 
     /* This method is to get All Categories Ordered By Name and returns list of CategoryEntity
     If error throws exception with error code and error message.
     */
     public List<CategoryEntity> getAllCategoriesOrderedByName() {
+        //Calls getAllCategoriesOrderedByName of categoryDao to get list of CategoryEntity
         List<CategoryEntity> categoryEntities = categoryDao.getAllCategoriesOrderedByName();
         return categoryEntities;
     }
@@ -44,16 +58,17 @@ public class CategoryService {
     If error throws exception with error code and error message.
     */
     public CategoryEntity getCategoryById(String categoryUuid) throws CategoryNotFoundException {
-        if(categoryUuid == null || categoryUuid == "") {
-            //Checking for categoryUuid to be null or empty to throw exception.
+        if(categoryUuid == null || categoryUuid == ""){ //Checking for categoryUuid to be null or empty to throw exception.
             throw new CategoryNotFoundException("CNF-001","Category id field should not be empty");
         }
+
         //Calls getCategoryByUuid of categoryDao to get CategoryEntity
         CategoryEntity categoryEntity = categoryDao.getCategoryByUuid(categoryUuid);
 
-        if(categoryEntity == null ) {
+        if(categoryEntity == null){ //Checking for categoryEntity to be null or empty to throw exception.
             throw new CategoryNotFoundException("CNF-002","No category by this id");
         }
+
         return categoryEntity;
     }
 }
