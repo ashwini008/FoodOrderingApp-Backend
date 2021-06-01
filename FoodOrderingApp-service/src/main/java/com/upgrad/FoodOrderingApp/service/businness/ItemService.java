df9d585dff6c542989b8ee545952443d1ffaf1ed
Soumya Ranjan Tripathy <sibun.soumya@gmail.com>
2021-06-02 01:29:45 +0530

    added functionality for catagory dependents

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/ItemService.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/ItemService.java
@@ -1,12 +1,20 @@
 package com.upgrad.FoodOrderingApp.service.businness;
 
+import com.upgrad.FoodOrderingApp.service.common.UtilityProvider;
 import com.upgrad.FoodOrderingApp.service.dao.ItemDao;
+import com.upgrad.FoodOrderingApp.service.dao.OrderDao;
+import com.upgrad.FoodOrderingApp.service.dao.OrderItemDao;
 import com.upgrad.FoodOrderingApp.service.entity.ItemEntity;
+import com.upgrad.FoodOrderingApp.service.entity.OrderItemEntity;
+import com.upgrad.FoodOrderingApp.service.entity.OrdersEntity;
 import com.upgrad.FoodOrderingApp.service.entity.RestaurantEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class ItemService {
@@ -14,7 +22,57 @@ public class ItemService {
     @Autowired
     private ItemDao itemDao;
 
-    public List<ItemEntity> getItemsByPopularity(final RestaurantEntity restaurantEntity) {
-        return itemDao.getItemsByPopularity(restaurantEntity.getId());
+    @Autowired
+    private OrderDao orderDao;
+
+    @Autowired
+    private OrderItemDao orderItemDao;
+
+    @Autowired
+    private UtilityProvider utilityProvider;
+
+    /* This method is to get Items By Popularity and returns list of ItemEntity it takes restaurantEntity as input.
+     */
+    public List<ItemEntity> getItemsByPopularity(RestaurantEntity restaurantEntity) {
+
+        //Calls getOrdersByRestaurant method of orderDao to get the  OrdersEntity
+        List <OrdersEntity> ordersEntities = orderDao.getOrdersByRestaurant(restaurantEntity);
+
+        //Creating list of ItemEntity which are ordered from the restaurant.
+        List <ItemEntity> itemEntities = new LinkedList<>();
+
+        //Looping in for each ordersEntity in ordersEntities to get the corresponding orders
+        ordersEntities.forEach(ordersEntity -> {
+            //Calls getItemsByOrders method of orderItemDao to get the  OrderItemEntity
+            List <OrderItemEntity> orderItemEntities = orderItemDao.getItemsByOrders(ordersEntity);
+            orderItemEntities.forEach(orderItemEntity -> { //Looping in to get each tem from the OrderItemEntity.
+                itemEntities.add(orderItemEntity.getItem());
+            });
+        });
+
+        //Creating a HashMap to count the frequency of the order.
+        Map<String,Integer> itemCountMap = new HashMap<String,Integer>();
+        itemEntities.forEach(itemEntity -> { //Looping in to count the frequency of Item ordered correspondingly updating the count.
+            Integer count = itemCountMap.get(itemEntity.getUuid());
+            itemCountMap.put(itemEntity.getUuid(),(count == null) ? 1 : count+1);
+        });
+
+        //Calls sortMapByValues method of uitilityProvider and get sorted map by value.
+        Map<String,Integer> sortedItemCountMap = utilityProvider.sortMapByValues(itemCountMap);
+
+        //Creating the top 5 Itementity list
+        List<ItemEntity> sortedItemEntites = new LinkedList<>();
+        Integer count = 0;
+        for(Map.Entry<String,Integer> item:sortedItemCountMap.entrySet()){
+            if(count < 5) {
+                //Calls getItemByUUID to get the Itemtentity
+                sortedItemEntites.add(itemDao.getItemByUUID(item.getKey()));
+                count = count+1;
+            }else{
+                break;
+            }
+        }
+
+        return sortedItemEntites;
     }
 }
